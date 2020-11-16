e6ac9938f8a2cebbdb39dfefd521d98c55c7901f
tamanugi <tamanugi@gmail.com>
2020-11-17 00:46:27 +0900

    wip

--- src/eventPage.ts
+++ src/eventPage.ts
@@ -1,11 +1,43 @@
-// Listen to messages sent from other parts of the extension.
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    // onMessage must return "true" if response is async.
-    let isResponseAsync = false;
+class MessageListener {
+    listeners = {}
 
-    if (request.popupMounted) {
-        console.log('eventPage notified that Popup.tsx has mounted.');
+    add(action, func) {
+        this.listeners[action] = func
     }
 
-    return isResponseAsync;
-});
+    listen(msg, sender, sendResponse) {
+        const func = this.listeners[msg.action]
+        if (func) {
+            func(msg, sender, sendResponse)
+        }
+        return true
+    }
+}
+
+const listener = new MessageListener()
+listener.add('fetchInfo', async (msg, sender, sendResponse) => {
+  const source = await fetch("").then(e => e.text())
+  const sections = new DOMParser().parseFromString(source, "text/html").body.getElementsByTagName("section")
+  const text = [...(sections as any)]
+     .filter(e => e.innerHTML.includes("問題文") || e.innerHTML.includes("制約"))
+     .map(e => e.innerHTML as string)
+     .map(txt => {
+         return txt
+            .replace("\n<h3>問題文</h3>", "[*** 問題文]\n")
+            .replace("<h3>制約</h3>", "[*** 制約]")
+            .replace(/<p>/g, "").replace(/<\/p>/g, "")
+            .replace(/<var>/g, "[$ ").replace(/<\/var>/g, " ]")
+            .replace(/<li>/g, "").replace(/<\/li>/g, "")
+            .replace(/<ul>/g, "").replace(/<\/ul>/g, "")
+            .replace(/<code>/g, "`").replace(/<\/code>/g, "`")
+            .replace(/&lt;/g, "<")
+     })
+     .join("")
+
+  sendResponse({text: text })
+})
+
+console.log("hogehoge")
+
+chrome.runtime.onMessage.addListener(listener.listen.bind(listener))
+
--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,11 +1,94 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect, useLayoutEffect } from 'react';
 import "./Popup.scss";
 
-export default function Popup() {
-  useEffect(() => {
-    // Example of how to send a message to eventPage.ts.
-    chrome.runtime.sendMessage({ popupMounted: true });
-  }, []);
+interface TaskInfo {
+  contestName?: string
+  problemName?: string
+  url?: string
+  taskBody?: string
+}
 
-  return <div className="popupContainer">Hello, world!</div>;
+const onScraptButton = (project, contestName: string, problemName: string, url: string, taskBody: string) => {
+
+  const title = contestName + ' ' + problemName
+  const body = `[${problemName} ${url}]\n\n${taskBody}\n-------`
+
+  const scrapBoxUrl = `https://scrapbox.io/${project}/${encodeURIComponent(title)}?body=${encodeURIComponent(body)}`
+  chrome.tabs.create({
+    url: scrapBoxUrl
+  })
+}
+
+export default function Popup() {
+
+  const [contestName, setContestName] = useState('')
+  const [problemName, setProblemName] = useState('')
+  const [url, setUrl] = useState('')
+  const [taskBody, setTaskBody] = useState('')
+  const [projects, setProjects] = useState([])
+  const [selectedProject, setSelectedProject] = useState('')
+
+  useLayoutEffect(() => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      console.log(tabs[0])
+
+      
+      setContestName(/\/contests\/(.*)\/tasks/.exec(tabs[0].url)[1].toUpperCase())
+      setProblemName(tabs[0].title)
+      setUrl(tabs[0].url)
+      
+      chrome.tabs.sendMessage(tabs[0].id, {
+        action: "fetchInfo"
+      }, (res) => {
+        if (res) {
+          setTaskBody(res.text)
+        }
+      });
+    });
+  }, [])
+
+  useLayoutEffect(() => {
+    fetch('https://scrapbox.io/api/projects')
+      .then(async (res) => {
+        const r = await res.json()
+        console.log(r)
+        setProjects(r.projects)
+        setSelectedProject(r.projects[4].name)
+      })
+  }, [])
+
+  return (
+    <div>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css"></link>
+      <div className="field">
+        <label className="label">Scrapbox Project</label>
+        <div className="select">
+          <select value={selectedProject} onChange={e => setSelectedProject(e.target.value)}>
+            {projects.map(p =>
+              <option key={p.id} value={p.name}>{p.displayName}</option>
+            )}
+          </select>
+        </div>
+      </div>
+      <div className="field">
+        <label className="label">Title</label>
+        <div className="content">
+            {contestName} {problemName}
+        </div>
+      </div>
+
+      <div className="field">
+        <label className="label">URL</label>
+        <div className="content">
+          {url}
+        </div>
+      </div>
+
+      <div className="field is-grouped is-grouped-centered">
+        <div className="control">
+          <button className="button is-primary" onClick={() => onScraptButton(selectedProject, contestName, problemName, url, taskBody)}>SCRAP!</button>
+        </div>
+      </div>
+    </div>
+  )
 }
